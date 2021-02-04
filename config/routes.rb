987664a42e38b3475897f71d38e2179851b07cbc
yoneyama-taiga <tai03.ga07@gmail.comgit config --global user.name yoney>
2021-02-04 19:39:11 +0900

    “[add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_scope :customer do
+    root "customers/sessions#new"
+  end
+
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions', # 会員ログイン画面
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations' # 会員登録画面、会員パスワード編集画面
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
